40506c1bfa8aa1d837711652416b08c2f0e47a95
Davi Campos <arlocm@gmail.com>
2012-11-21 11:13:30 -0200

    organizing files

--- manifests/pythonbrasil.pp
+++ manifests/pythonbrasil.pp
@@ -30,29 +30,6 @@ class pythonbrasil::bd {
   }
 }
 
-class pythonbrasil_django_requirements {
-  package { "libmysqlclient-dev":
-    ensure => installed,
-  }
-
-  package { "python-dev":
-    ensure => installed,
-  }
-
-  package { "libxml2-dev":
-    ensure => installed,
-  }
-
-  package { "libxslt1-dev":
-    ensure => installed,
-  }
-
-  package { "build-essential":
-    ensure => installed,
-  }
-
-}
-
 class pythonbrasil::site {
   include nginx
   include mysql
@@ -88,6 +65,29 @@ class pythonbrasil::proxy($backends) {
 
   nginx::site { 'pythonbrasil-proxy':
     content => template('demo/nginx/proxy.erb')
+  }
+
+}
+
+class pythonbrasil_django_requirements {
+  package { "libmysqlclient-dev":
+    ensure => installed,
+  }
+
+  package { "python-dev":
+    ensure => installed,
+  }
+
+  package { "libxml2-dev":
+    ensure => installed,
+  }
+
+  package { "libxslt1-dev":
+    ensure => installed,
+  }
+
+  package { "build-essential":
+    ensure => installed,
   }
 
 }
