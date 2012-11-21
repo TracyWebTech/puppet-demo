09907ed70ca0f3eaec722918ff7bf2c1d4c1c0d7
Davi Campos <arlocm@gmail.com>
2012-11-21 11:22:07 -0200

    Fixing node names

--- manifests/site.pp
+++ manifests/site.pp
@@ -2,14 +2,14 @@ node default  {
   include pythonbrasil
 }
 
-node "agent1.tracy.com.br" inherits default { # MySQL
+node "puppet1.tracy.com.br" inherits default { # MySQL
   include pythonbrasil::bd
 }
 
-node "agent2.tracy.com.br", "agent3.tracy.com.br" inherits default {
+node "puppet2.tracy.com.br", "puppet3.tracy.com.br" inherits default {
   include pythonbrasil::site
 }
 
-node "agent4.tracy.com.br" inherits default {
+node "puppet4.tracy.com.br" inherits default {
   include pythonbrasil::proxy
 }
